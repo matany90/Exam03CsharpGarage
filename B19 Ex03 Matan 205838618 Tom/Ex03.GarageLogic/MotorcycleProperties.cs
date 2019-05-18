d93a3e0e739f03f9106037038d4a56d39dbb89c4
TomLezmy <49162863+TomLezmy@users.noreply.github.com>
2019-05-18 19:02:07 +0300

    Added MotorcycleProperties

---
+++ B19 Ex03 Matan 205838618 Tom/Ex03.GarageLogic/MotorcycleProperties.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ex03.GarageLogic
+{
+    class MotorcycleProperties
+    {
+        protected eLicenseTypes m_LicenseType;
+        protected int m_EngineVolume;
+
+        public MotorcycleProperties(eLicenseTypes i_LicenseType, int i_EngineVolume)
+        {
+            m_LicenseType = i_LicenseType;
+            m_EngineVolume = i_EngineVolume;
+        }
+    }
+}
