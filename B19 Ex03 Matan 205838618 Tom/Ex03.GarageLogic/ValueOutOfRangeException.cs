99367431ac656fa7565532e7be43d71ae22ff7ad
TomLezmy <49162863+TomLezmy@users.noreply.github.com>
2019-05-18 17:57:54 +0300

    Exception update

--- B19 Ex03 Matan 205838618 Tom/Ex03.GarageLogic/ValueOutOfRangeException.cs
+++ B19 Ex03 Matan 205838618 Tom/Ex03.GarageLogic/ValueOutOfRangeException.cs
@@ -6,12 +6,18 @@ namespace Ex03.GarageLogic
 {
     class ValueOutOfRangeException : Exception
     {
+        private float m_MaxValue;
+        private float m_MinValue;
+
         public ValueOutOfRangeException(string i_Message) : base(i_Message)
         {
 
         }
 
-        public ValueOutOfRangeException(string i_Message, Exception innerException)
-    : base(i_Message, innerException) { }
+        public ValueOutOfRangeException(Exception i_InnerException, float i_MaxValue, float i_MinValue) : base(string.Format("Error, the value is out of range between {0} and {1}", i_MinValue, i_MaxValue), i_InnerException)
+        {
+            m_MaxValue = i_MaxValue;
+            m_MinValue = i_MinValue;
+        }
     }
 }
