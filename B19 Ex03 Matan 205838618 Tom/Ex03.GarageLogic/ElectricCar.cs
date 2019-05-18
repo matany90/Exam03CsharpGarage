1c53e7b35df60f228de1ff821c51eb8f3650ee5d
TomLezmy <49162863+TomLezmy@users.noreply.github.com>
2019-05-18 19:07:36 +0300

    Update ElectricCar.cs

--- B19 Ex03 Matan 205838618 Tom/Ex03.GarageLogic/ElectricCar.cs
+++ B19 Ex03 Matan 205838618 Tom/Ex03.GarageLogic/ElectricCar.cs
@@ -4,9 +4,11 @@ namespace Ex03.GarageLogic
 {
     class ElectricCar : ElectricVehicle
     {
-        public ElectricCar(string i_ModelName, string i_LicenseNumber, float i_EnergyLeftByPercentage, float i_BatteryTimeLeftByHours, float i_MaxBatteryTime) : base(i_ModelName, i_LicenseNumber, i_EnergyLeftByPercentage, i_BatteryTimeLeftByHours, i_MaxBatteryTime)
+        private CarProperties m_CarProperties;
+
+        public ElectricCar(string i_ModelName, string i_LicenseNumber, float i_EnergyLeftByPercentage, float i_BatteryTimeLeftByHours, float i_MaxBatteryTime, eCarColor i_Color, eDoorsNumber i_DoorNumber) : base(i_ModelName, i_LicenseNumber, i_EnergyLeftByPercentage, i_BatteryTimeLeftByHours, i_MaxBatteryTime)
         {
-            
+            m_CarProperties = new CarProperties(i_Color, i_DoorNumber);
         }
 
         public override void show() // to delete
