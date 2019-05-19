4ab45a9401906948efb96d2fd4057642fcee2df5
MATAN\matan <matany90@gmail.com>
2019-05-19 12:25:36 +0300

    Add Owner

---
+++ B19 Ex03 Matan 205838618 Tom/Ex03.GarageLogic/Owner.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex03.GarageLogic
+{
+    class Owner
+    {
+        private string m_OwnerName;
+        private string m_OwnerPhone;
+        private eVehicleStatus m_VehicleStatus;
+
+        public Owner(string i_OwnerName, string i_OwnerPhone, eVehicleStatus i_VehicleStatus)
+        {
+            m_OwnerName = i_OwnerName;
+            m_OwnerPhone = i_OwnerPhone;
+            m_VehicleStatus = i_VehicleStatus;
+        }
+    }
+}
