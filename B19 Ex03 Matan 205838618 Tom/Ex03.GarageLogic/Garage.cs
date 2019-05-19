23768d800a614c1f0236c416570572ade79c44f7
TomLezmy <49162863+TomLezmy@users.noreply.github.com>
2019-05-19 12:40:45 +0300

    added garage

---
+++ B19 Ex03 Matan 205838618 Tom/Ex03.GarageLogic/Garage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex03.GarageLogic
+{
+    class Garage
+    {
+        private Dictionary<Owner, VehicleInGarage> m_VehiclesInGarage;
+        public bool AddCarToGarage(Owner i_Owner, Vehicle i_Vehicle)
+        {
+        }
+
+        public List<int> ShowLicenseNumbers()//add parameters
+        {
+        }
+
+        public void ChangeVehicleState(string i_LicensePlate, int i_State)
+        {
+        }
+
+        public void FillWheelsToMax(string i_LicenseNumber)
+        {
+        }
+    }
+}
