30b2f73b38fd1a03447112e60fa0f2aa87e7def5
TomLezmy <49162863+TomLezmy@users.noreply.github.com>
2019-05-19 12:25:59 +0300

    Added vehicleingarage

---
+++ B19 Ex03 Matan 205838618 Tom/Ex03.GarageLogic/VehicleInGarage.cs
@@ -0,0 +1,34 @@
+﻿namespace Ex03.GarageLogic
+{
+    class VehicleInGarage
+    {
+        private Vehicle m_Vehicle;
+        private eVehicleConditions m_VehicleState;
+
+        public VehicleInGarage(Vehicle i_Vehicle)
+        {
+            m_Vehicle = i_Vehicle;
+            m_VehicleState = eVehicleConditions.InRepair;
+        }
+
+        public Vehicle Vehicle
+        {
+            get
+            {
+                return m_Vehicle;
+            }
+        }
+
+        public eVehicleConditions VehicleState
+        {
+            get
+            {
+                return m_VehicleState;
+            }
+            set
+            {
+                m_VehicleState = value;
+            }
+        }
+    }
+}
